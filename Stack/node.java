237cb08d27264a8458310995a1594ba0c1649f27
Mhmd Shami <mhmddshamii@gmail.com>
2025-03-22 21:46:24 +0200

    stack

---
+++ Stack/node.java
@@ -0,0 +1,9 @@
+package Stack;
+
+public class node<type> {
+    public type value;
+    public node<type> next;
+    public node(type value){
+        this.value = value;
+    }
+}
--- Stack/stack.java
+++ Stack/stack.java
@@ -1,44 +1,20 @@
 package Stack;
 
-import LinkedList.linkedList;
-import LinkedList.node;
-
 public class stack<type> {
     
-    linkedList<type> list;
-
-    public stack(){
-        list = new linkedList<>();
-    }
-
-    //adding
-    public void push(type value)
-    {
-        list.addFirst(value);
-    }
-
-    //removing
-    public node<type> pop()
-    {
-
-        if (!this.isEmpty()) {
-            return list.removeFirst();
-        }
-        return null;
-    }
-
-    //view
-    public type top()
-    {
-        if (!this.isEmpty()) {
-            return list.getHead().value;
-        }
-        return null;
-    }
-
-    public boolean isEmpty()
-    {
-        return list.getHead() == null;
+    private node<type> top;
+    private int height;
+    
+    public stack(type value){
+        node<type> newNode = new node<>(value);
+        top = newNode;
+        height = 1;
     }
     
+    public node<type> getTop() {
+        return top;
+    }
+    public int getHeight() {
+        return height;
+    }
 }
